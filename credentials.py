107df70f04da58fa448c2b90c0ba7994ca1535ce
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-04-24 13:15:45 +0300

    __init__ to define properties of new Credentials

--- credentials.py
+++ credentials.py
@@ -9,4 +9,15 @@ class Credentials:
 
     credentials_list = []
 
-    
+    def __init__(self, website_name, user_name, password):
+        '''
+        __init__ to define properties of new Credentials
+        Args:
+            website_name: Website in which the credentials belong to.
+            user_name: username for websites account
+            password: password for the account
+        '''
+
+        self.website_name = website_name
+        self.user_name = user_name
+        self.password = password 
