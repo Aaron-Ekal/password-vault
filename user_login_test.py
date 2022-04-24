307042986c6655c9579a5d75a51fdc393ea1aafb
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-04-24 13:11:33 +0300

    test initialization

--- user_login_test.py
+++ user_login_test.py
@@ -8,3 +8,19 @@ class TestUser(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class creates test cases 
     '''
+
+    def setUp(self):
+        '''
+        Method run before each test to build a start
+        '''
+        self.new_user = User("Aaron", "Ekal", "Tellem", "tellem@ent.com", "123qwerty")
+
+    def test__init__(self):
+        '''
+        test initialization to test object instantiation
+        '''
+        self.assertEqual(self.new_user.first_name, "Aaron")
+        self.assertEqual(self.new_user.sur_name, "Ekal")
+        self.assertEqual(self.new_user.user_name, "Tellem")
+        self.assertEqual(self.new_user.email, "tellem@ent.com")
+        self.assertEqual(self.new_user.password, "123qwerty")
