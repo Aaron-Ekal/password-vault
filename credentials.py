273b2e04f45f7d9aa22f430d673db8ac6471b2a8
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-04-24 13:17:51 +0300

    verifying website_name

--- credentials.py
+++ credentials.py
@@ -21,3 +21,29 @@ class Credentials:
         self.website_name = website_name
         self.user_name = user_name
         self.password = password 
+
+
+    def save_credentials(self):
+        '''
+        saves new instances to credentials_list array
+        '''
+        Credentials.credentials_list.append(self)
+
+    def delete_credentials(self):
+        '''
+        removes existing instances from credentials_list
+        '''
+        Credentials.credentials_list.remove(self)
+
+    @classmethod
+    def find_by_site_name(cls, website_name):
+        '''
+        this method takes in website_name & returns the credentials tat match the website_name
+        Args:
+            website_name: site name to search for
+        Returns:
+            login credentials of the website
+        '''
+        for credential in cls.credentials_list:
+            if credential.website_name == website_name:
+                return credential
