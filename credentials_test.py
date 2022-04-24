ae24f456913d53f0be3481a404858cfb9a71bbdb
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-04-24 13:24:13 +0300

    creates test cases & checks object instantiation

--- credentials_test.py
+++ credentials_test.py
@@ -10,3 +10,17 @@ class TestUser(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class creates test cases
     '''
+
+    def setUp(self):
+        '''
+        Method to create test cases for the credentials class
+        '''
+        self.new_credentials = Credentials("Twitter", "AaronEkal", "qwerty123")
+
+    def test__init__(self):
+        '''
+        test to check object instatiation
+        '''
+        self.assertEqual(self.new_credentials.website_name, "Twitter")
+        self.assertEqual(self.new_credentials.user_name, "AaronEkal")
+        self.assertEqual(self.new_credentials.password, "qwerty123")
