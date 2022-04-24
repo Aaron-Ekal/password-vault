75f312efc33920b1aa2424a49349330b13a39554
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-04-24 13:22:39 +0300

    imports for Credentials and TestUser class

---
+++ credentials_test.py
@@ -0,0 +1,12 @@
+import unittest
+from credentials import Credentials
+
+
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class to define test cases for the User class
+
+    Args:
+        unittest.TestCase: TestCase class creates test cases
+    '''
