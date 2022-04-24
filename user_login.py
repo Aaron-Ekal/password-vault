7824845c8f5af968e0a4f3415fbb42533ee936bc
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-04-24 12:59:26 +0300

    initial emply list and __init__ method

--- user_login.py
+++ user_login.py
@@ -2,4 +2,21 @@ class User:
     '''
     class to generate a new user instance
     '''
-    
+
+    user_list = []
+
+    def __init__(self, first_name, sur_name, user_name, email, password):
+        '''
+        __init__ method to define properties for object user
+        Args:
+            first_name: New user first name.
+            sur_name: New user last name.
+            email: New user email address.
+            password: New user account password.
+        '''
+
+        self.first_name = first_name
+        self.sur_name = sur_name
+        self.user_name = user_name
+        self.email = email
+        self.password = password
