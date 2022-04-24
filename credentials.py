c7bf567b941bbb0df7b845a7e6db1b8b1f23ce4c
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-04-24 13:14:52 +0300

    credentials class initial empty  storage list

---
+++ credentials.py
@@ -0,0 +1,12 @@
+import random, string
+from user_login import User
+
+
+class Credentials:
+    '''
+    Credentials class to create new credentials instances
+    '''
+
+    credentials_list = []
+
+    
